78587eb0c000eee49ab790d7cdea689fb1eeb0cf
Tatiana5 <shehovtzova.tanya@yandex.ru>
2025-05-23 16:15:34 +0300

    Multilang fix

---
+++ m_Qev.php
@@ -0,0 +1,9 @@
+<?php
+/**
+*
+*  @package Antibot 42
+*  @copyright (c) 2019 Татьяна5 and Nekstati
+*  @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+ namespace nekstati\antibot42; use Symfony\Component\HttpKernel\KernelEvents; use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent; use Symfony\Component\HttpKernel\Exception\NotFoundHttpException; use Symfony\Component\EventDispatcher\EventSubscriberInterface; class m_Qev implements EventSubscriberInterface { protected $z_YM6, $KUm8b; public function __construct(\nekstati\antibot42\QT0JZ $i1eot, \nekstati\antibot42\DhEYv $u3y6o) { $this->z_YM6 = $i1eot; $this->KUm8b = $u3y6o; } public function khytcO($YNvaA) { goto QKJ1F; vxhGl: return; goto s_jwm; PMPNi: $WVOcu = $YNvaA->getException(); goto XU17W; XU17W: $dBlOO = $YNvaA->getRequest(); goto vz3zD; bmos2: wIPQO: goto eZ8CO; QKJ1F: if ($YNvaA instanceof GetResponseForExceptionEvent) { goto wIPQO; } goto oufg4; vz3zD: $m84Cn = $this->KUm8b->Z9SRp; goto WVOuW; eZ8CO: if ($this->KUm8b->rwM7y["\x6d\x65\x74\150\x6f\x64\x33"]) { goto DsZ2A; } goto vxhGl; NlJzz: $YNvaA->setResponse($no0DT); goto LlShw; LMU46: $no0DT = $this->z_YM6->lPb27($m84Cn); goto NlJzz; WVOuW: if (!($WVOcu instanceof NotFoundHttpException && $WVOcu->getStatusCode() == "\x34\60\x34" && $dBlOO->getPathInfo() == "\57" . $m84Cn)) { goto EH5iV; } goto LMU46; LlShw: EH5iV: goto i0KAu; s_jwm: DsZ2A: goto PMPNi; oufg4: return; goto bmos2; i0KAu: } public static function getSubscribedEvents() { goto Wx9zm; Wx9zm: if (!defined("\101\104\x4d\x49\116\137\123\124\101\122\124")) { goto CO49N; } goto vLK8O; vLK8O: return []; goto iMxF1; He1vL: return [KernelEvents::EXCEPTION => ["\153\x68\171\x74\x63\x4f", 2001]]; goto bfacO; iMxF1: CO49N: goto He1vL; bfacO: } }
--- xjBma.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-*
-*  @package Antibot 42
-*  @copyright (c) 2019 Татьяна5 and Nekstati
-*  @license GNU General Public License, version 2 (GPL-2.0)
-*
-*/
- namespace nekstati\antibot42; use Symfony\Component\HttpKernel\KernelEvents; use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent; use Symfony\Component\HttpKernel\Exception\NotFoundHttpException; use Symfony\Component\EventDispatcher\EventSubscriberInterface; class xjBma implements EventSubscriberInterface { protected $VPhaN, $MqMg6; public function __construct(\nekstati\antibot42\WmAC_ $a8MAl, \nekstati\antibot42\AcCvK $MdhuK) { $this->VPhaN = $a8MAl; $this->MqMg6 = $MdhuK; } public function FOhDig($tYT0T) { goto kwvxI; LuN5M: $NqYle = $this->VPhaN->JOpIi($CUjBx); goto UYCvE; S2e1Q: $CUjBx = $this->MqMg6->wGp20; goto XBGLA; gzt_f: mf04g: goto UbVU1; UYCvE: $tYT0T->setResponse($NqYle); goto ITIW6; Bt30Z: CFXiq: goto HewtO; VFozC: return; goto gzt_f; ITIW6: PwSKR: goto YmF_a; aBeM6: $tbt3R = $tYT0T->getRequest(); goto S2e1Q; kwvxI: if ($tYT0T instanceof GetResponseForExceptionEvent) { goto mf04g; } goto VFozC; HewtO: $M1dRl = $tYT0T->getException(); goto aBeM6; UbVU1: if ($this->MqMg6->bKZC1["\155\145\x74\150\157\144\x33"]) { goto CFXiq; } goto K5M89; K5M89: return; goto Bt30Z; XBGLA: if (!($M1dRl instanceof NotFoundHttpException && $M1dRl->getStatusCode() == "\64\x30\64" && $tbt3R->getPathInfo() == "\57" . $CUjBx)) { goto PwSKR; } goto LuN5M; YmF_a: } public static function getSubscribedEvents() { goto w9PDz; plfht: return [KernelEvents::EXCEPTION => ["\x46\x4f\150\x44\151\x67", 2001]]; goto Iefdd; Qm36P: skPLT: goto plfht; w9PDz: if (!defined("\x41\x44\x4d\111\116\x5f\123\124\x41\122\124")) { goto skPLT; } goto dNgP4; dNgP4: return []; goto Qm36P; Iefdd: } }
